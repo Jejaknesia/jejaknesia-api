6eabf369f1c105674abb17f0e581ce37106c6306
Sujaruu <ytfuga02@gmail.com>
2023-05-31 11:59:54 +0700

    add: user authentication

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@ const connection = require('../utils/db');
 
 const getAllRecordsHandler = async (request, h) => {
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM records', (error, results, fields) => {
+    connection.query('SELECT * FROM tourism', (error, results, fields) => {
       if (error) {
         console.error(error);
         reject(error);
@@ -12,15 +12,49 @@ const getAllRecordsHandler = async (request, h) => {
         status: 'success',
         data: results
       }).code(200);
-
-      // const response = h.response({
-      //   status: 'success',
-      //   data: results
-      // }).code(200);
-
       resolve(response);
     });
   });
 };
 
-module.exports = { getAllRecordsHandler };
+const registerHandler = async (request, h) => {
+  const { username, password } = request.payload;
+  return new Promise((resolve, reject) => {
+    connection.query(`INSERT INTO users (username, password) VALUES ('${username}', '${password}')`, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      }
+
+      const response = h.response({
+        status: 'success',
+        data: results
+      }).code(200);
+      resolve(response);
+    });
+  });
+};
+const loginHandler = async (request, h) => {
+  const { username, password } = request.payload;
+
+  // Mencetak input dari user yang sedang login
+  console.log("Username:", username);
+  console.log("Password:", password);
+
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM users WHERE username='${username}' AND password='${password}'`, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      }
+
+      const response = h.response({
+        status: 'login success',
+      }).code(200);
+      resolve(response);
+    });
+  });
+}
+
+
+module.exports = { getAllRecordsHandler, registerHandler, loginHandler };
