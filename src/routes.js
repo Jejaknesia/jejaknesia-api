6dd3a7a12f6aeeacdba00235d1682965cfb8ce8a
Sujaruu <ytfuga02@gmail.com>
2023-05-31 11:58:48 +0700

    update: route register and login

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
-const { getAllRecordsHandler } = require('./handler');
+const { getAllRecordsHandler, registerHandler, loginHandler } = require('./handler');
 
 const routes = [
   {
     path: '/',
     method: 'GET',
     handler: getAllRecordsHandler
+  },
+  {
+    path: '/register',
+    method: 'POST',
+    handler: registerHandler
+  },
+  {
+    path: '/login',
+    method: 'POST',
+    handler: loginHandler
   }
 ]
 
