ff64677c57ee46781cfe1e586f511325aa509e5a
Alham <alhammanazil18@gmail.com>
2023-05-31 13:18:54 +0700

    update bug fixing in login handler

--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,19 @@
-const connection = require('../utils/db');
+const connection = require("../utils/db");
 
 const getAllRecordsHandler = async (request, h) => {
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM tourism', (error, results, fields) => {
+    connection.query("SELECT * FROM tourism", (error, results, fields) => {
       if (error) {
         console.error(error);
         reject(error);
       }
 
-      const response = h.response({
-        status: 'success',
-        data: results
-      }).code(200);
+      const response = h
+        .response({
+          status: "success",
+          data: results,
+        })
+        .code(200);
       resolve(response);
     });
   });
@@ -20,18 +22,23 @@ const getAllRecordsHandler = async (request, h) => {
 const registerHandler = async (request, h) => {
   const { username, password } = request.payload;
   return new Promise((resolve, reject) => {
-    connection.query(`INSERT INTO users (username, password) VALUES ('${username}', '${password}')`, (error, results, fields) => {
-      if (error) {
-        console.error(error);
-        reject(error);
-      }
+    connection.query(
+      `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`,
+      (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        }
 
-      const response = h.response({
-        status: 'success',
-        data: results
-      }).code(200);
-      resolve(response);
-    });
+        const response = h
+          .response({
+            status: "success",
+            data: results,
+          })
+          .code(200);
+        resolve(response);
+      }
+    );
   });
 };
 const loginHandler = async (request, h) => {
@@ -42,19 +49,36 @@ const loginHandler = async (request, h) => {
   console.log("Password:", password);
 
   return new Promise((resolve, reject) => {
-    connection.query(`SELECT * FROM users WHERE username='${username}' AND password='${password}'`, (error, results, fields) => {
-      if (error) {
-        console.error(error);
-        reject(error);
+    const authUsers = connection.query(
+      `SELECT * FROM users WHERE username='${username}' AND password='${password}'`,
+      (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else if (results.length === 0) {
+          // Jika tidak ada hasil dari query, artinya kombinasi username dan password tidak ditemukan
+          const response = h
+            .response({
+              status: "login failed",
+              message: "Invalid username or password",
+            })
+            .code(401);
+          resolve(response);
+        } else {
+          const response = h
+            .response({
+              status: "login success",
+            })
+            .code(200);
+          resolve(response);
+        }
       }
-
-      const response = h.response({
-        status: 'login success',
-      }).code(200);
-      resolve(response);
-    });
+    );
   });
-}
+};
 
+const authUsers = (username, password) => {
+  !username || !password ? false : true;
+};
 
 module.exports = { getAllRecordsHandler, registerHandler, loginHandler };
