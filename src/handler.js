7b08c3a8567d74968fd691451547d6397efae55f
Alham <alhammanazil18@gmail.com>
2023-06-01 17:50:47 +0700

    update update register and login in handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,84 +1,153 @@
 const connection = require("../utils/db");
 
-const getAllRecordsHandler = async (request, h) => {
-  return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM tourism", (error, results, fields) => {
-      if (error) {
-        console.error(error);
-        reject(error);
-      }
-
-      const response = h
-        .response({
-          status: "success",
-          data: results,
-        })
-        .code(200);
-      resolve(response);
-    });
-  });
+const isValidEmail = (email) => {
+  // Regex untuk memvalidasi format email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 };
 
-const registerHandler = async (request, h) => {
-  const { email, password } = request.payload;
-  return new Promise((resolve, reject) => {
-    connection.query(
-      `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`,
-      (error, results, fields) => {
+const getAllRecordsHandler = async (request, h) => {
+  try {
+    const results = await new Promise((resolve, reject) => {
+      connection.query("SELECT * FROM tourism", (error, results, fields) => {
         if (error) {
           console.error(error);
           reject(error);
         }
+        resolve(results);
+      });
+    });
 
-        const response = h
-          .response({
-            status: "success",
-            data: results,
-          })
-          .code(200);
-        resolve(response);
-      }
-    );
-  });
+    return h
+      .response({
+        status: "success",
+        data: results,
+      })
+      .code(200);
+  } catch (error) {
+    return h
+      .response({
+        status: "error",
+        message: "Internal Server Error",
+      })
+      .code(500);
+  }
 };
+
+// REGISTER
+const registerHandler = async (request, h) => {
+  const { email, password } = request.payload;
+
+  if (!email || !password) {
+    return h
+      .response({
+        status: "error",
+        message: "Email and password are required fields",
+      })
+      .code(400);
+  }
+
+  if (!isValidEmail(email)) {
+    return h
+      .response({
+        status: "error",
+        message: "Invalid email format",
+      })
+      .code(400);
+  }
+
+  try {
+    await new Promise((resolve, reject) => {
+      connection.query(
+        `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`,
+        (error, results, fields) => {
+          if (error) {
+            console.error(error);
+            if (error.code === "ER_DUP_ENTRY") {
+              return reject({
+                status: "error",
+                message: "Email is already registered",
+              });
+            }
+            return reject({
+              status: "error",
+              message: "Internal Server Error",
+            });
+          }
+          resolve(results);
+        }
+      );
+    });
+
+    return h
+      .response({
+        status: "success",
+        message: "User registered successfully",
+      })
+      .code(200);
+  } catch (error) {
+    return h
+      .response({
+        status: "error",
+        message: error.message || "Internal Server Error",
+      })
+      .code(error.status || 500);
+  }
+};
+// REGISTER
+
+// LOGIN
 const loginHandler = async (request, h) => {
   const { email, password } = request.payload;
 
-  // Mencetak input dari user yang sedang login
-  console.log("email:", email);
-  console.log("Password:", password);
+  if (!isValidEmail(email)) {
+    return h
+      .response({
+        status: "error",
+        message: "Invalid email format",
+      })
+      .code(400);
+  }
 
-  return new Promise((resolve, reject) => {
-    const authUsers = connection.query(
-      `SELECT * FROM users WHERE email='${email}' AND password='${password}'`,
-      (error, results, fields) => {
-        if (error) {
-          console.error(error);
-          reject(error);
-        } else if (results.length === 0) {
-          // Jika tidak ada hasil dari query, artinya kombinasi email dan password tidak ditemukan
-          const response = h
-            .response({
-              status: "login failed",
-              message: "Invalid email or password",
-            })
-            .code(401);
-          resolve(response);
-        } else {
-          const response = h
-            .response({
-              status: "login success",
-            })
-            .code(200);
-          resolve(response);
+  try {
+    const results = await new Promise((resolve, reject) => {
+      connection.query(
+        `SELECT * FROM users WHERE email='${email}' AND password='${password}'`,
+        (error, results, fields) => {
+          if (error) {
+            console.error(error);
+            reject(error);
+          } else if (results.length === 0) {
+            // Jika tidak ada hasil dari query, artinya kombinasi email dan password tidak ditemukan
+            const response = h
+              .response({
+                status: "login failed",
+                message: "Invalid email or password",
+              })
+              .code(401);
+            resolve(response);
+          } else {
+            const response = h
+              .response({
+                status: "login success",
+              })
+              .code(200);
+            resolve(response);
+          }
         }
-      }
-    );
-  });
-};
+      );
+    });
 
-const authUsers = (email, password) => {
-  !email || !password ? false : true;
+    return results;
+  } catch (error) {
+    return h
+      .response({
+        status: "error",
+        message: "Internal Server Error",
+      })
+      .code(500);
+  }
 };
+// LOGIN
 
 module.exports = { getAllRecordsHandler, registerHandler, loginHandler };
